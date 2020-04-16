3ab9c7f8200539b6d3c2aab6e61c54c46f8e7ea9
Ilya <ilya3434@gmail.com>
2020-04-16 21:56:12 +0300

    создание файла utmblock.js

---
+++ utmblock.js
@@ -0,0 +1,6 @@
+   $(document).ready(function() {
+   		url = document.location.href;
+           if ( url.match(/utm_term=block/)){
+               window.location = "http://block-city.plp7.ru/"; // Ссылка для перенаправления.
+           }
+   });
