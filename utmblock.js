9254b6f6a5ac22babd3eacf585b8405f6225b627
Ilya <ilya3434@gmail.com>
2020-04-16 22:11:43 +0300

    добавление комментария в скрипте

--- utmblock.js
+++ utmblock.js
@@ -1,6 +1,6 @@
    $(document).ready(function() {
    		url = document.location.href;
-           if ( url.match(/utm_term=block/)){
+           if ( url.match(/utm_term=block/)){ // метку можно изменить на свою
                window.location = "http://block-city.plp7.ru/"; // Ссылка для перенаправления.
            }
    });
